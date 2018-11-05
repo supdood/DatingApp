b5205791c359cc7b88e63612bc38e586fd9050b6
Fetsko, Ethan <EFetsko@geico.com>
2018-11-05 09:54:45 -0500

    Added login, registration, and routing

---
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  model: any = {};
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  register() {
+    this.authService.register(this.model).subscribe(next => {
+      console.log('Registration successful');
+    }, error => {
+      console.log(error.error);
+    });
+  }
+
+  cancel() {
+    console.log('cancelled');
+  }
+}
