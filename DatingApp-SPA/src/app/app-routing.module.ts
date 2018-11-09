3ace3de47a160f2ad00635ae2032b1a71b9f91a3
Fetsko, Ethan <EFetsko@geico.com>
2018-11-09 10:46:53 -0500

    Added member edit functionality

--- DatingApp-SPA/src/app/app-routing.module.ts
+++ DatingApp-SPA/src/app/app-routing.module.ts
@@ -9,6 +9,9 @@ import { AuthGuard } from './_guards/auth.guard';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 import { MemberListResolver } from './_resolvers/member-list.resolver';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { MemberEditResolver } from './_resolvers/member-edit.resolver';
+import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -20,6 +23,8 @@ export const routes: Routes = [
     children: [
       { path: 'members', component: MemberListComponent, resolve: { users: MemberListResolver } },
       { path: 'members/:id', component: MemberDetailComponent, resolve: { user: MemberDetailResolver } },
+      { path: 'member/edit', component: MemberEditComponent,
+        resolve: { user: MemberEditResolver }, canDeactivate: [PreventUnsavedChanges]},
       { path: 'lists', component: ListsComponent },
       { path: 'messages', component: MessagesComponent }
     ]
--- DatingApp.API/Helpers/AutoMapperProfiles.cs
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -25,6 +25,7 @@ namespace DatingApp.API.Helpers
                     opt.ResolveUsing(d => d.DateOfBirth.CalculateAge());
                 });
             CreateMap<Photo, PhotoForDetailedDto>();
+            CreateMap<UserForUpdateDto, User>();
         }
     }
 }
